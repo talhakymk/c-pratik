0503f594c95be634a15d44b90c616f31ccc14874
talhakymak <talhakymk@gmail.com>
2023-02-04 17:35:58 +0300

    program.cs

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace test
+{
+
+    class Program 
+    {
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("isminizi girin");
+            string? isim = Console.ReadLine();
+            Console.WriteLine("soyisminizi girin");
+            string? suyisim = Console.ReadLine();
+            
+            Console.WriteLine("merhaba " + isim + " " + suyisim);
+        }
+    }
+}
+
